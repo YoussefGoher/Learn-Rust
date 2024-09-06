1018e862047a7f834695bc705134c2b26958df12
youssefgoher <eng_youssef_goher@outlook.com>
2024-09-06 11:35:43 +0300

    test of rustfmt

---
+++ hello_world/hello_world.rs
@@ -0,0 +1,17 @@
+//rustfmt hello_world.rs
+
+fn main() {
+    println!("hello,world from rust");println!("rust need semicolon only for separate");
+    println!("hello,world from rust")
+}
+/*
+rustfmt:
+1- Reformat to Best Practices:
+rustfmt automatically reformats Rust code to adhere to best practices and style guidelines. It ensures consistent code style, alignment, and formatting according to Rust's conventions.
+
+2- Remove Whitespace:
+rustfmt will also clean up unnecessary whitespace, such as trailing spaces and extra newlines.
+
+3- Convert Tabs to 4 Spaces:
+By default, rustfmt uses 4 spaces for indentation, which aligns with Rust’s style guide. It will automatically convert tabs to 4 spaces during formatting.
+*/
