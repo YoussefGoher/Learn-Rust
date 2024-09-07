8355edd34a6b61a863092c15cc5e693d97947a2c
youssefgoher <eng_youssef_goher@outlook.com>
2024-09-07 23:04:13 +0300

    using "cargo run" and "cargo run --release"

--- hello_world/hello_world_via_cargo/src/main.rs
+++ hello_world/hello_world_via_cargo/src/main.rs
@@ -1,6 +1,8 @@
 /*
     use cargo to create project "cargo new hello_world_via_cargo (take snack case)"
     you can build using "cargo build" or release using "cargo build --release"
+    you can run using "cargo run" to build and run in debug mode
+    or you can run using "cargo run --release" to build and run in release mode
 */ 
 fn main() {
     println!("Hello, world!");
