ae6c8f0bb2f5033308b65b995ade29e0284ac30d
youssefgoher <eng_youssef_goher@outlook.com>
2024-09-07 22:56:55 +0300

    build or release using cargo

--- hello_world/hello_world_via_cargo/src/main.rs
+++ hello_world/hello_world_via_cargo/src/main.rs
@@ -1,5 +1,6 @@
 /*
     use cargo to create project "cargo new hello_world_via_cargo (take snack case)"
+    you can build using "cargo build" or release using "cargo build --release"
 */ 
 fn main() {
     println!("Hello, world!");
