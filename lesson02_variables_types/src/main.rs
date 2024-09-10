038de714264a82fe24105dc52db39b252d8563e6
youssefgoher <eng_youssef_goher@outlook.com>
2024-09-10 06:29:14 +0300

    Techniques in Rust variables

--- lesson02_variables_types/src/main.rs
+++ lesson02_variables_types/src/main.rs
@@ -3,10 +3,11 @@
                           unsigned  -> {u8,u16,u32,u64,u128,usize"word size"}
 */
 fn main() {
-    println!("\n Hello, Rust variables");
+    println!("\nHello, Rust variables");
     //demo_integers();
     //demo_floats();
-    demo_other_simple_types();
+    //demo_other_simple_types();
+    demo_techniques();
 }
 
 //create new function to test integer types
@@ -54,7 +55,7 @@ fn demo_floats() {
     println!("\n speed of light {0}, {0:e}, {0:.4e}", f4);
 }
 */
-
+/*
 fn demo_other_simple_types() {
     let is_youssef: bool = true;
     let can_sing: bool = false;
@@ -74,4 +75,43 @@ fn demo_other_simple_types() {
         favourite_emoji,
         std::mem::size_of::<char>()
     );
+}
+*/
+fn demo_techniques() {
+
+    // Rust can infer types.
+    let a = -12345;
+    let b = 3.14;
+    let c = 'X';
+    println!("\na is {}, b is {}, c is {}", a, b, c);
+
+    // Variables are immutable by default.
+    let d = 0;
+    //d = 1;
+    println!("d is {}", d);
+
+    // You must use the mut qualifier to make a variable mutable.
+    let mut e = 0;
+    println!("e originally is {}", e);
+    e = 1;
+    println!("e afterwards is {}", e);
+ 
+    // If you don't use a variable, prefix name with _ to avoid compiler warning.
+    let _f = 0;
+
+    // You can cast using the "as" keyword.
+    let g = 3.99;
+    let h = g as i32;
+    println!("g is {}, h is {}", g, h);
+
+    // Rust enables you to redeclare a variable in the current scope. This is called shadowing and it's quite cool.
+    let num = String::from("12345");
+    println!("num as a string is {}", num);
+    let num = 12345;
+    println!("num+1 as a number is {}", num + 1);
+
+    // You can define compile-time constants. You must specify the type btw.
+    const SECONDS_IN_HOUR: i32 = 3_600;
+    const SECONDS_IN_DAY: i32 = 24 * SECONDS_IN_HOUR;
+    println!("There are {} seconds in a day", SECONDS_IN_DAY);  
 }
