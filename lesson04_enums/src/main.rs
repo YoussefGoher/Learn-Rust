d96657c0b8d69938d156981116c4eca10382fdb3
youssefgoher <eng_youssef_goher@outlook.com>
2024-09-12 03:19:41 +0300

    using Option enum

--- lesson04_enums/src/main.rs
+++ lesson04_enums/src/main.rs
@@ -8,7 +8,8 @@ use mytypes::{Colour, HouseLocation};
 
 fn main() {
     //demo_simple_enums();
-    demo_enum_with_data();
+    //demo_enum_with_data();
+    demo_using_option_enum();
 }
 
 fn demo_simple_enums() {
@@ -37,4 +38,26 @@ fn demo_enum_with_data() {
         "Btw the size of HouseLocation is {} bytes",
         std::mem::size_of::<HouseLocation>()
     );
+}
+
+fn demo_using_option_enum() {
+    println!("\nDemo Using enum Option type");
+    //let secs: Option<u32> = sec_of_day(23, 59, 59);
+    let secs: Option<u32> = sec_of_day(23, 59, 60);
+    match secs {
+        Some(s) => println!("second of day using match: is {}", s),
+        None => println!("second of day using match: no value available"),
+    }
+    println!(
+        "second of day using unwrap_or method: {}",
+        secs.unwrap_or(0)
+    );
+}
+
+fn sec_of_day(hours: u32, minutes: u32, seconds: u32) -> Option<u32> {
+    if hours <= 23 && minutes <= 59 && seconds <= 59 {
+        return Option::Some(hours * 3600 + minutes * 60 + seconds);
+    } else {
+        return Option::None;
+    }
 }
