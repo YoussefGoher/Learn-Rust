3c084d5f1c84d549a7449b91bf39619e095e94a6
youssefgoher <eng_youssef_goher@outlook.com>
2024-09-19 02:06:23 +0300

    String Handling in Rust

--- lesson06_scope_ownership/src/main.rs
+++ lesson06_scope_ownership/src/main.rs
@@ -1,11 +1,13 @@
 //mod demo_locals;
 //mod demo_static_local;
 //mod demo_static_global; 
-mod demo_static_mutable;
+//mod demo_static_mutable;
+mod demo_string_handling;
 fn main() {
     //println!("\nBtw GLOBAL_MESSAGE is {}",demo_static_global::GLOBAL_MESSAGE);
     //demo_locals::do_it();
     //demo_static_local::do_it();
     //demo_static_global::do_it();
-    demo_static_mutable::do_it();
+    //demo_static_mutable::do_it();
+    demo_string_handling::do_it();
 }
