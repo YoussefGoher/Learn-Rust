8c06352f3750dc2795dd3df9b8eb8ab87877989a
youssefgoher <eng_youssef_goher@outlook.com>
2024-09-19 00:27:02 +0300

    Static Mutable in Rust

--- lesson06_scope_ownership/src/main.rs
+++ lesson06_scope_ownership/src/main.rs
@@ -1,9 +1,11 @@
 //mod demo_locals;
 //mod demo_static_local;
-mod demo_static_global; 
+//mod demo_static_global; 
+mod demo_static_mutable;
 fn main() {
-    println!("\nBtw GLOBAL_MESSAGE is {}",demo_static_global::GLOBAL_MESSAGE);
+    //println!("\nBtw GLOBAL_MESSAGE is {}",demo_static_global::GLOBAL_MESSAGE);
     //demo_locals::do_it();
     //demo_static_local::do_it();
-    demo_static_global::do_it();
+    //demo_static_global::do_it();
+    demo_static_mutable::do_it();
 }
