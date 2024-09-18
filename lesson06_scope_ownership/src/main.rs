23c1771d2f1a5af9a9753a3743b2e42cb5273e8e
youssefgoher <eng_youssef_goher@outlook.com>
2024-09-19 02:17:35 +0300

    Copy vs Moving in Rust

--- lesson06_scope_ownership/src/main.rs
+++ lesson06_scope_ownership/src/main.rs
@@ -2,12 +2,14 @@
 //mod demo_static_local;
 //mod demo_static_global; 
 //mod demo_static_mutable;
-mod demo_string_handling;
+//mod demo_string_handling;
+mod demo_copying_vs_moving;
 fn main() {
     //println!("\nBtw GLOBAL_MESSAGE is {}",demo_static_global::GLOBAL_MESSAGE);
     //demo_locals::do_it();
     //demo_static_local::do_it();
     //demo_static_global::do_it();
     //demo_static_mutable::do_it();
-    demo_string_handling::do_it();
+    //demo_string_handling::do_it();
+    demo_copying_vs_moving::do_it();
 }
