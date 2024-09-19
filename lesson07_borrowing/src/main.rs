075a9b9096071a0d8a901fd005622c4d03f43764
youssefgoher <eng_youssef_goher@outlook.com>
2024-09-19 04:42:40 +0300

    Borrow Checker in Rust

--- lesson07_borrowing/src/main.rs
+++ lesson07_borrowing/src/main.rs
@@ -1,4 +1,6 @@
-mod demo_simple_borrowing;
+//mod demo_simple_borrowing;
+mod demo_borrow_checker;
 fn main() {
-    demo_simple_borrowing::do_it();
+    //demo_simple_borrowing::do_it();
+    demo_borrow_checker::do_it();
 }
